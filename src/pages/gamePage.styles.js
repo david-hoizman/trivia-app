42c679f40ec38973226b02b9feeeae808222d2bf
david <ahdavid2022@gmail.com>
2024-12-17 10:40:56 +0200

    clean dev

--- src/pages/gamePage.styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const PageWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-`;
--- src/utils/shuffleUtils.js
+++
@@ -1,5 +0,0 @@
-// פונקציה שמערבבת את המערך באופן רנדומלי
-export const shuffleArray = (array) => {
-    return array.sort(() => Math.random() - 0.5);
-  };
-  
